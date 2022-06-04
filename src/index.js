3afc8b0d9f43d218fc5edc4bde80e5c433f80aff
Diechabral <th3d1eg0master@gmail.com>
2022-06-03 22:35:32 -0300

    change name of imports on index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { BrowserRouter, Routes, Route, } from "react-router-dom";
-import App from "./pages/App";
+import App from "./pages/app";
 import Map from "./pages/map";
 
 
